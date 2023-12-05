05b613820c74711846d4c83b63803ee38d8a9058
Javier Leguina <javier@leguina.com>
2023-12-05 22:41:17 +0100

    Cleanup

--- lua/custom/plugins/neoformat.lua
+++
@@ -1,8 +0,0 @@
-return {
-    'sbdchd/neoformat',
-    -- Must run: 'let g:neoformat_run_all_formatters = 1'
-    -- This doesnt work: 
-    -- config = function()
-    --     vim.g.neoformat_run_all_formatters = 1
-    -- end
-}
