502b87ccb8bcb5ec3b1965f383e991eeaad03fbd
Javier Leguina <javier@leguina.com>
2023-12-05 19:26:35 +0100

    Weird

--- lua/custom/plugins/formatter.lua
+++ lua/custom/plugins/formatter.lua
@@ -4,34 +4,34 @@
 -- See the kickstart.nvim README for more information
 {'mhartington/formatter.nvim'}
 
--- [[ Configure Formatter ]]
--- Utilities for creating configurations
-local util = require "formatter.util"
+-- -- [[ Configure Formatter ]]
+-- -- Utilities for creating configurations
+-- local util = require "formatter.util"
 
--- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
-require("formatter").setup {
-  -- Enable or disable logging
-  logging = true,
-  -- Set the log level
-  log_level = vim.log.levels.WARN,
-  -- All formatter configurations are opt-in
-  filetype = {
-    -- Formatter configurations for filetype "lua" go here
-    -- and will be executed in order
-    python = {
-      -- "formatter.filetypes.python" defines default configurations for the
-      -- "python" filetype
-      require("formatter.filetypes.python").isort,
-      require("formatter.filetypes.python").black
-      require("formatter.filetypes.python").autoflake
-    }
+-- -- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
+-- require("formatter").setup {
+--   -- Enable or disable logging
+--   logging = true,
+--   -- Set the log level
+--   log_level = vim.log.levels.WARN,
+--   -- All formatter configurations are opt-in
+--   filetype = {
+--     -- Formatter configurations for filetype "lua" go here
+--     -- and will be executed in order
+--     python = {
+--       -- "formatter.filetypes.python" defines default configurations for the
+--       -- "python" filetype
+--       require("formatter.filetypes.python").isort,
+--       require("formatter.filetypes.python").black
+--       require("formatter.filetypes.python").autoflake
+--     }
 
-    -- Use the special "*" filetype for defining formatter configurations on
-    -- any filetype
-    ["*"] = {
-      -- "formatter.filetypes.any" defines default configurations for any
-      -- filetype
-      require("formatter.filetypes.any").remove_trailing_whitespace
-    }
-  }
-}
+--     -- Use the special "*" filetype for defining formatter configurations on
+--     -- any filetype
+--     ["*"] = {
+--       -- "formatter.filetypes.any" defines default configurations for any
+--       -- filetype
+--       require("formatter.filetypes.any").remove_trailing_whitespace
+--     }
+--   }
+-- }
