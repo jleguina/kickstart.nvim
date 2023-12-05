467a0e2c1fc12be39926842f20167edf6aeb2424
Javier Leguina <javier@leguina.com>
2023-12-05 22:38:03 +0100

    Conform

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -1,4 +1,21 @@
 return {
   'stevearc/conform.nvim',
   opts = {},
+
+  config = function ()
+    require("conform").setup {
+        formatters_by_ft = {
+            lua = { "stylua" },
+            -- Conform will run multiple formatters sequentially
+            python = { "ruff_fix", "ruff_format" },
+            -- Use a sub-list to run only the first available formatter
+            javascript = { { "prettierd", "prettier" } },
+        },
+        format_on_save = {
+            -- These options will be passed to conform.format()
+            timeout_ms = 500,
+            lsp_fallback = true,
+        },
+    }
+  end
 }
